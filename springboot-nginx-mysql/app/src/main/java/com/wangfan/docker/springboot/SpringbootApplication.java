80b219a400dfe5cefe800183b85eeb60d9a9fdd9
wangfan <wangfan@ejiayou.com>
2019-04-15 08:13:39 +0800

    rdsbak

--- springboot-nginx-mysql/app/src/main/java/com/wangfan/docker/springboot/SpringbootApplication.java
+++ springboot-nginx-mysql/app/src/main/java/com/wangfan/docker/springboot/SpringbootApplication.java
@@ -2,10 +2,11 @@ package com.wangfan.docker.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan("com.wangfan.docker.springboot")
 public class SpringbootApplication {
 
